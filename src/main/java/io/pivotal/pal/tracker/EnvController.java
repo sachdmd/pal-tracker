0aa6ac0722f8b5dd41d88a1a03359ed734c86041
rbs <manish.sachdeva@rbs.com>
2019-08-27 10:18:06 +0530

    commited

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,8 +1,40 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class EnvController {
+
+    String port;
+   String memoryLimit;
+    String cfInstanceIndex;
+    String cfInstanceAddr;
+
+    @Autowired
+    public EnvController(@Value ("${$port:NOT SET}") String port,@Value ("${memory.limit:NOT SET}") String memoryLimit,@Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+                  @Value ("${cf.instance.addr:NOT SET}") String cfInstanceAddr){
+
+       this.port=port;
+        this.memoryLimit=memoryLimit;
+        this.cfInstanceIndex=cfInstanceIndex;
+        this.cfInstanceAddr=cfInstanceAddr;
+  }
+
+@GetMapping("/env")
+public Map<String, String> getEnv() throws Exception {
+    Map<String, String> env = new HashMap<>();
+    env.put("PORT",port);
+    env.put("MEMORY_LIMIT",memoryLimit);
+    env.put("CF_INSTANCE_INDEX",cfInstanceIndex);
+    env.put("CF_INSTANCE_ADDR",cfInstanceAddr);
+    return env;
+}
+
 
 }
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @Value("${welcome.message}")
+
     private String sayHello;
 
+    @Autowired
+    public WelcomeController(@Value("${welcome.message}") String sayHello){
+        this.sayHello=sayHello;
+
+    }
     @GetMapping("/")
     public String sayHello() {
         return sayHello;
